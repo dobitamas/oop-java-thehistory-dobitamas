f6989dbcaf5096c3d0f192e1475b2c248dc37a10
Szabolcs Gyorki <sz.gyorki@gmail.com>
2018-06-05 15:36:56 +0200

    Modified import statements

--- src/main/java/com/codecool/thehistory/TheHistoryArrayList.java
+++ src/main/java/com/codecool/thehistory/TheHistoryArrayList.java
@@ -1,6 +1,9 @@
 package com.codecool.thehistory;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ListIterator;
 
 public class TheHistoryArrayList implements TheHistory {
     /**
